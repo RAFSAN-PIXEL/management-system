d2194b40e51446a369eade42d372c535aa8663ba
RAFSAN-PIXEL <skrafsan06@gmail.com>
2024-12-31 20:47:08 +0600

    Update Main part3.cpp

--- Main part3.cpp
+++ Main part3.cpp
@@ -1,1 +1,47 @@
+int main()
+{
+  TrafficManagementSystem tsm;
+int choice;
+
+do
+{
+cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+cout<<">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl; 
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                            WELCOME TO                                                          '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                SMART TRAFFIC MANAGEMENT SYSTEM                                                 '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                   PRESS YOUR OPTION :-                                                                                         '**"<<endl;
+cout<<"**'                                   1.Record Of Vehicle                                                                          '**"<<endl;
+cout<<"**'                                   2.Record Of Violation                                                                        '**"<<endl;
+cout<<"**'                                   3.Search For Vehicle                                                                         '**"<<endl;
+cout<<"**'                                   4.Add Traffic Booth                                                                          '**"<<endl;
+cout<<"**'                                   5.Simulate Booth Entry                                                                       '**"<<endl;
+cout<<"**'                                   6.Simulate Booth Entry                                                                       '**"<<endl;
+cout<<"**'                                   7.Display Emergency Info                                                                     '**"<<endl;
+cout<<"**'                                   8.Display All Vehicles                                                                       '**"<<endl;
+cout<<"**'                                   9.Display All Booths                                                                         '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                   Enter Your Choice __                                                                                         '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'                                                                                                                                '**"<<endl;
+cout<<"**'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+cout<<"**'>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"<<endl;
+
+cin >> choice;
+cin.ignore();
+
+switch (choice)
+{
+case 1:
 
